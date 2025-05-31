3fc39f5e71535fcc929bbaa861c10af25b579ca5
HopeBridge <hopebridgeacademy@gmail.com>
2025-05-31 18:55:03 +0430

    Add files via upload

--- script.js
+++ script.js
@@ -173,10 +173,10 @@ function verifyCertification(event) {
 
     if (student) {
         if (student.status === 'enrolled') {
-            resultElement.textContent = `Student ${student.name} is currently enrolled in the ${student.department} department.`;
+            resultElement.textContent = `Student, ${student.name} is currently enrolled in the ${student.department} department.`;
             resultElement.classList.add('status-enrolled');
         } else if (student.status === 'graduated') {
-            resultElement.textContent = `Student ${student.name} has graduated from the ${student.department} department in ${student.graduationYear}.`;
+            resultElement.textContent = `Mr./Ms. ${student.name} graduated from the ${student.department} department in ${student.graduationYear}.`;
             resultElement.classList.add('status-graduated');
         }
     } else {
